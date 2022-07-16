bd419dd9fce34666e20b1c6094aa7f84a167611b
EC2 Default User <ec2-user@ip-172-31-17-201.ap-northeast-1.compute.internal>
2022-07-16 07:54:21 +0000

    index追加途中

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'books/show'
   get 'books/edit'
   get 'books/destroy'
-  get 'books/index'
+  get 'books'=>'books#index'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
