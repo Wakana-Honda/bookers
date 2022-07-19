85780d541972d99e80e4ff2e71a7ecd15df9c69d
EC2 Default User <ec2-user@ip-172-31-17-201.ap-northeast-1.compute.internal>
2022-07-19 12:27:10 +0000

    style.cssの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,12 @@ class BooksController < ApplicationController
     @books=Book.all
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title,:body)
