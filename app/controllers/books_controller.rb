e6417b584e20a8381871daedccf137e85b6682e7
EC2 Default User <ec2-user@ip-172-31-17-201.ap-northeast-1.compute.internal>
2022-07-22 10:01:16 +0000

    フラッシュメッセージの色付け

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully created"
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice:"Book was successfully created"
     else
-      flash[:alret] = "error"
-      @books = Book.all
-      render "index"
+      render :index
     end
   end
 
@@ -29,12 +26,9 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     book = Book.find(params[:id])
     if book.update(book_params)
-      flash[:notice] = "Book was successfully updated"
-      redirect_to book_path(book.id)
+      redirect_to book_path(book.id), notice:"Book was successfully updated"
     else
-      flash[:alret] = "error"
-      @books = Book.all
-      render "edit"
+      render:edit
     end
   end
 
