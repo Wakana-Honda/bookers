dbe7b9855d13176bcb435db9a64723e8450ff7e6
EC2 Default User <ec2-user@ip-172-31-17-201.ap-northeast-1.compute.internal>
2022-07-22 05:52:38 +0000

    フラッシュメッセージの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,14 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      flash[:alret] = "error"
+      @books = Book.all
+      render "index"
+    end
   end
 
   def show
@@ -20,9 +26,16 @@ class BooksController < ApplicationController
   end
   
   def update
+    @book = Book.new(book_params)
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(book.id)
+    else
+      flash[:alret] = "error"
+      @books = Book.all
+      render "edit"
+    end
   end
 
   def index
